75b540854d5db496e7ae768dcbcf13ccff81cd97
dheysson <dheyssonsousasilva@gmail.com>
2025-06-29 01:31:17 -0400

    i hope its the initial and final commit

---
+++ app.js
@@ -0,0 +1,75 @@
+let numberIsValid = false;
+const textInput = document.querySelector("#msg");
+
+const telInput = document.querySelector("#tel");
+telInput.addEventListener("keyup", (ev) => {
+  handleInput(ev);
+});
+
+const btn = document.querySelector("button");
+btn.addEventListener("click", () => {
+  if (!numberIsValid) {
+    Toastify({
+      text: "⚠️Verifique o número de telefone e tente novamente",
+      duration: 3000,
+      newWindow: true,
+      gravity: "bottom",
+      position: "right",
+      stopOnFocus: true,
+      style: {
+        padding: "2rem",
+        color: "#000000",
+        background: "#ffffff",
+      },
+    }).showToast();
+    return;
+  }
+  try {
+    navigator.clipboard.writeText(
+      generateLink(telInput.value, textInput.value)
+    );
+  } catch (err) {
+    console.log(err);
+  } finally {
+    Toastify({
+      text: "✅O link foi copiado para área de transferência",
+      duration: 3000,
+      newWindow: true,
+      gravity: "bottom",
+      position: "right",
+      stopOnFocus: true,
+      style: {
+        padding: "2rem",
+        color: "#000000",
+        background: "#ffffff",
+      },
+    }).showToast();
+  }
+  console.log(generateLink(telInput.value, textInput.value));
+});
+
+function handleInput(ev) {
+  ev.preventDefault();
+
+  let telInputValue = ev.target.value;
+  if (telInputValue.length == 11) {
+    let str = telInputValue;
+    let regex = /[0-9]{2}[9]{1}[0-9]{8}/;
+    console.log(regex.test(str));
+    if (!regex.test(str)) {
+      ev.target.setAttribute("style", "border-color: rgb(220, 20, 60);");
+      numberIsValid = false;
+      return;
+    }
+    if (regex.test(str)) {
+      numberIsValid = true;
+      ev.target.setAttribute("style", "border-color: rgb(0, 0, 0);");
+    }
+  }
+}
+
+function generateLink(number, message) {
+  let formattedMessage = message.replaceAll(" ", "+");
+  if (message) return `https://wa.me/55${number}/?text=${formattedMessage}`;
+  return `https://wa.me/55${number}/`;
+}
